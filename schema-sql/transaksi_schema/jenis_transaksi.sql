8c1a72e4a0e6948aa4d4eb258f4090f199537aa9
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 06:53:11 +0700

    Add jenis_transaksi table schema to transaksi_schema

---
+++ schema-sql/transaksi_schema/jenis_transaksi.sql
@@ -0,0 +1,9 @@
+-- jenis transaksi
+CREATE TYPE JENIS_TRANSAKSI_ENUM AS ENUM (
+	'beli_film', 'bayar_membership', 'ganti_membership',
+	'pembatalan_membership', 'memebership_diakhiri');
+CREATE TABLE IF NOT EXISTS jenis_transaksi (
+	kode_jenis_transaksi serial PRIMARY KEY NOT NULL,
+	jenis_transaksi JENIS_TRANSAKSI_ENUM NOT NULL,
+	keterangan_transaksi VARCHAR (255)
+);
