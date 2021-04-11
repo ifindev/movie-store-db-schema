816f57077a6c559fb16c32f776e37b9987d6c526
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-11 21:07:36 +0700

    Add tabel membership -- status membership

---
+++ schema-sql/tabel_memb_status_membership.sql
@@ -0,0 +1,6 @@
+-- status membership
+CREATE TYPE STATUS_MEMBERSHIP_ENUM AS ENUM ('aktif', 'nonaktif');
+CREATE TABLE IF NOT EXISTS status_membership (
+	id_status_membership serial PRIMARY KEY NOT NULL,
+	status_membership STATUS_MEMBERSHIP_ENUM NOT NULL
+);
