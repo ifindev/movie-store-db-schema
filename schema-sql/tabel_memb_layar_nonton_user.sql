e476c7ab2f5477348e8ce07ef17604b295ee36ba
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-11 21:11:10 +0700

    Add tabel membership - layar nonton user

---
+++ schema-sql/tabel_memb_layar_nonton_user.sql
@@ -0,0 +1,12 @@
+-- layar nonton user
+CREATE TABLE IF NOT EXISTS layar_nonton_user (
+	id_layar_nonton serial PRIMARY KEY NOT NULL,
+	id_membership INT NOT NULL,
+	id_jenis_layar INT NOT NULL,
+	waktu_data_dibuat TIMESTAMPTZ NOT NULL,
+	waktu_data_diperbarui TIMESTAMPTZ NOT NULL,
+	FOREIGN key (id_membership)
+		REFERENCES data_membership (id_membership),
+	FOREIGN key (id_jenis_layar)
+		REFERENCES jenis_layar (id_jenis_layar)
+);
