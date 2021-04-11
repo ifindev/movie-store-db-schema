7e08ad9592c956b68d3869ecb714b9de4abc6235
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-11 21:10:18 +0700

    Add tabel membership -- resolusi maks user

---
+++ schema-sql/tabel_memb_resolusi_maks_user.sql
@@ -0,0 +1,12 @@
+-- resolusi maksimum user
+CREATE TABLE IF NOT EXISTS resolusi_maksimum_user (
+	id_resolusi_maksimum serial PRIMARY KEY NOT NULL,
+	id_membership INT NOT NULL,
+	id_jenis_resolusi_maksimum INT NOT NULL,
+	waktu_data_dibuat TIMESTAMPTZ NOT NULL,
+	waktu_data_diperbarui TIMESTAMPTZ NOT NULL,
+	FOREIGN key (id_membership)
+		REFERENCES data_membership (id_membership),
+	FOREIGN key (id_jenis_resolusi_maksimum)
+		REFERENCES jenis_resolusi_maksimum (id_jenis_resolusi_maksimum)
+);
