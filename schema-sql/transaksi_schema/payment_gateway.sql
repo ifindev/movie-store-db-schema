b17ac4a4ab3b5ebb12809fefd3fb0bf16fd09c01
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 06:52:39 +0700

    Add payment_gateway table schema to transaksi_schema

---
+++ schema-sql/transaksi_schema/payment_gateway.sql
@@ -0,0 +1,7 @@
+-- payment gateway
+CREATE TYPE PAYMENT_GATEWAY_ENUM AS ENUM ('Mandiri', 'BCA', 'BNI', 'Stripe', 'OVO', 'Gopay');
+CREATE TABLE IF NOT EXISTS payment_gateway (
+	id_payment_gateway serial PRIMARY KEY NOT NULL,
+	payment_gateway PAYMENT_GATEWAY_ENUM NOT NULL,
+	keterangan_payment_gateway VARCHAR (255)
+);
