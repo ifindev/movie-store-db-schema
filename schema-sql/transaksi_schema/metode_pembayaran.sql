b38570c5928a60337527094d7e731dc6621ca4f8
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 06:52:01 +0700

    Add metode_pembayaran table schema to transaksi_schema

---
+++ schema-sql/transaksi_schema/metode_pembayaran.sql
@@ -0,0 +1,7 @@
+-- metode pembayaran
+CREATE TYPE METODE_PEMBAYARAN_ENUM AS ENUM ('transfer_bank', 'kartu_kredit', 'e_money');
+CREATE TABLE IF NOT EXISTS metode_pembayaran (
+	id_metode_pembayaran serial PRIMARY KEY NOT NULL,
+	metode_pembayaran METODE_PEMBAYARAN_ENUM NOT NULL,
+	keterangan_metode_pembayaran VARCHAR (255)
+);
