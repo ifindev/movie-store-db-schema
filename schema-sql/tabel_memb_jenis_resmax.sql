0c3d9de04adc52be9e51cfa5741f0cbf5e68a33d
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-11 17:44:18 +0700

    sql schema for jenis resolusi maksimum layar

---
+++ schema-sql/tabel_memb_jenis_resmax.sql
@@ -0,0 +1,6 @@
+-- jenis resolusi maksimum
+CREATE TYPE JENIS_RESMAX_ENUM AS ENUM ('480p', '720p', '1080p', '4K+');
+CREATE TABLE IF NOT EXISTS jenis_resolusi_maksimum (
+	id_jenis_resolusi_maksimum serial PRIMARY KEY NOT NULL,
+	jenis_resolusi_maksimum JENIS_RESMAX_ENUM NOT NULL
+);
