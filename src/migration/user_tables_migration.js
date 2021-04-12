9d664cfcfee3f88358d594cb91f79fe2c035da45
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 07:46:20 +0700

    Add user table migrations import

---
+++ src/migration/user_tables_migration.js
@@ -0,0 +1,3 @@
+import { data_user } from '../schema/user_schema/data_user';
+import { riwayat_login_user } from '../schema/user_schema/riwayat_login_user';
+import { riwayat_logout_user } from '../schema/user_schema/riwayat_logout_user';
