55c79a10fcd2fbde6c22d1b9c7bc1e7c3cb79d23
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 07:15:19 +0700

    Add riwayat_nonton_user.js

---
+++ src/schema/user_schema/riwayat_nonton_user.js
@@ -0,0 +1,13 @@
+export const riwayat_nonton_user = `
+  CREATE TABLE IF NOT EXISTS riwayat_nonton_user (
+    id_riwayat_nonton serial PRIMARY KEY NOT NULL,
+    id_user INT NOT NULL,
+    id_film INT NOT NULL,
+    waktu_data_dibuat TIMESTAMPTZ NOT NULL,
+    waktu_data_diperbarui TIMESTAMPTZ NOT NULL,
+    FOREIGN key (id_user)
+      REFERENCES data_user (id_user),
+    FOREIGN key (id_film)
+      REFERENCES daftar_film (id_film)
+  );
+`;
