bdbe52cf99e711e33c41494d1324344f83e4bcba
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 07:19:11 +0700

    Add data_membership.js

---
+++ src/schema/membership_schema/data_membership.js
@@ -0,0 +1,16 @@
+export const data_membership = `
+  CREATE TABLE IF NOT EXISTS data_membership (
+    id_membership serial PRIMARY KEY NOT NULL,
+    id_user INT NOT NULL,
+    id_jenis_membership INT NOT NULL,
+    id_status_membership INT NOT NULL,
+    waktu_data_dibuat TIMESTAMPTZ NOT NULL,
+    waktu_data_diperbarui TIMESTAMPTZ NOT NULL,
+    FOREIGN key (id_user)
+      REFERENCES data_user (id_user),
+    FOREIGN key (id_jenis_membership)
+      REFERENCES jenis_membership (id_jenis_membership),
+    FOREIGN key (id_status_membership)
+      REFERENCES status_membership (id_status_membership)
+  );
+`;
