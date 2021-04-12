a2315d522fc21e5af4fc941c604e64afab5616cb
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 07:01:36 +0700

    Add daftar_film_berseri to movie_schema

---
+++ schema-sql/movie_schema/daftar_film_berseri.sql
@@ -0,0 +1,10 @@
+-- daftar film berseri
+CREATE TABLE IF NOT EXISTS daftar_film_berseri (
+	id_series serial PRIMARY KEY NOT NULL,
+	id_film INT NOT NULL,
+	nama_seri VARCHAR (100) NOT NULL,
+	season INT NOT NULL,
+	episode INT NOT NULL,
+	FOREIGN key (id_film)
+		REFERENCES daftar_film (id_film)
+);
