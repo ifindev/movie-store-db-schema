514a3fc37a092d5f34b3ee88c801a2d39fd80f92
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-04-12 07:39:21 +0700

    Add daftar_film.js

---
+++ src/schema/movie_schema/daftar_film.js
@@ -0,0 +1,12 @@
+export const daftar_film = `
+  CREATE TABLE IF NOT EXISTS daftar_film (
+    id_film serial PRIMARY KEY NOT NULL,
+    judul VARCHAR(100) NOT NULL,
+    tahun_rilis DATE NOT NULL,
+    rating_film DECIMAL (2,2),
+    link_thumbnail VARCHAR (255),
+    link_film VARCHAR (255),
+    lokasi_shooting VARCHAR (50),
+    bahasa VARCHAR (50)
+  );
+`;
